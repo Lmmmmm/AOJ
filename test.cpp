856e8f19f69274650105e44ecb8f1640d27d9671
李勐 <lm839377386@gmail.com>
2020-03-16 02:34:39 +0900

    aoj

--- test.cpp
+++ test.cpp
@@ -1,24 +1,27 @@
+#include <cmath>
+#include <iomanip>
 #include <iostream>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
 using namespace std;
- 
- 
-//  5
-//  0 1 2 3 4
-int main(){
-    string s;
-    cin >> s;
-    int flag=0;
-    for(int i = 0; i< s.length()-1;i++){
-        if(s[i]-'h'==0 && s[i+1]-'i'==0){
-            if(i-1==0 && i+2!=s.length()&& s[i+2]-'i'!=0) {flag=1;break;}
-            if(i-1!=0 && i+2==s.length()&& s[i-1]-'h'!=0) {flag=1;break;}
-            if(i-1!=0 && i+2!=s.length()&& s[i-1]-'h'!=0&&s[i+2]-'i'!=0) {flag=1;break;}
+
+int N;
+void dfs(string s, char mx)
+{
+    if (s.length() == N)
+    {
+        printf("%s\n", s.c_str());
+    }
+    else
+    {
+        for (char c = 'a'; c <= mx; c++)
+        {
+            dfs(s + c, ((c == mx) ? (char)(mx + 1) : mx));
         }
     }
-    if(flag == 1) cout << "Yes"<<endl;
-    else cout << "No"<<endl;
+}
+int main()
+{
+    cin >> N;
+    dfs("", 'a');
+
     return 0;
 }
--- test1.cpp
+++ test1.cpp
@@ -1,26 +1,10 @@
 #include <iostream>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-using namespace std;
+#include <cmath>
 
-int main(){
-    int a,b;
-    cin >> a >> b;
-    int a1 = (a/0.08)-1;//25
-    // if((double)a % 0.08 !=(double)0) a1++;
-    // int a2 = (a/0.07)+1;//28
-    int flag = 0;
-    if(a1<=100){
-        // for(int i = a1; i<=(a2 < 100 ? a2 :100); i++){
-        for(int i = a1; i<=100; i++){
-            if(int(i *0.1) == b && int(i*0.08 == a)) {
-                cout << i << endl;
-                flag = 1;
-                break;
-            }
-        }
-    }
-    if(flag == 0) cout<< "-1"<<endl;
-    return 0;
-}
+using namespace std;
+typedef long long ll;
+
+// int main(void)
+// {
+//    int 
+// }
